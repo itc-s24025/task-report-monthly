0ea428a41edf82d540e28bb94cd8c8d82b6ed67e
itc-s24013 <s24013@std.it-college.ac.jp>
2026-01-26 11:59:29 +0900

    todoテーブル定義

--- modules/todo.py
+++ modules/todo.py
@@ -1,8 +1,18 @@
 # models.py
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from .db_instance import db
 
-db = SQLAlchemy()  # ここで db を作る（中身は空っぽ）
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    title = db.Column(db.String(100), nullable=False)
+    memo = db.Column(db.String(100))
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime)
+    started_by = db.Column(db.DateTime)
+    ended_by = db.Column(db.DateTime)
+    duration_seconds = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
