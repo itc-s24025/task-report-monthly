2764d80d378d370f662dd9ec31bd1fe61920870c
itc-s24013 <s24013@std.it-college.ac.jp>
2026-02-02 10:22:35 +0900

    ルーティングの名前を変更

--- api/task.py
+++ api/task.py
@@ -1,9 +1,9 @@
 from flask import Blueprint,request, redirect, url_for
 from modules.todo import db, Todo
 
-bp = Blueprint('task', __name__)
+task_bp = Blueprint('task', __name__)
 
-@bp.route("/add", methods=["POST"])
+@task_bp.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
     start_time = request.form.get("start_time")
@@ -15,14 +15,14 @@ def add():
     db.session.commit()
     return redirect(url_for("home"))
 
-@bp.route("/delete/<int:todo_id>", methods=["POST"])
+@task_bp.route("/delete/<int:todo_id>", methods=["POST"])
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
 
-@bp.route("/update/<int:todo_id>", methods=["POST"])
+@task_bp.route("/update/<int:todo_id>", methods=["POST"])
 def update(todo_id):
     todo = Todo.query.get(todo_id)
     if todo is None:
