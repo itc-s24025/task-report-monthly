7bd67714024fd43de85c32c412fcd8c45b2c6677
itc-s24013 <s24013@std.it-college.ac.jp>
2026-02-13 11:18:57 +0900

    月次集計で月の作業時間(h[第一小数点まで])と作業した日数を返すように変更。 集計は実際に行ったタスクの時間で行う

--- app/routes/filter.py
+++ app/routes/filter.py
@@ -1,5 +1,5 @@
 from babel.messages.extract import extract
-from flask import Blueprint,request, redirect, url_for
+from flask import Blueprint,request, redirect, url_for, jsonify
 from sqlalchemy import func
 from app.models.task import Task
 from app import db
@@ -21,7 +21,8 @@ def monthly(year, month):
     month_todo = db.session.query(
         extract('year', Task.started_date).label('year'),
         extract('month', Task.started_date).label('month'),
-        func.sum(Task.duration_seconds).label('total_duration')
+        func.sum(Task.duration_seconds).label('total_duration'),
+        func.count(Task.id).label('total_day')
     ).filter(
         extract('year', Task.started_date) == year,
         extract('month', Task.started_date) == month,
@@ -32,4 +33,7 @@ def monthly(year, month):
     # month_todo の中身（イメージ）
     # (2026, 2, 3600.0)
 
-    return redirect("monthly.html", month_todo=month_todo)
+    return jsonify({
+        "total_hour": round(month_todo.total_duration / 60 , 1) if month_todo else 0,
+        "total_day": month_todo.total_day if month_todo else 0
+    })
