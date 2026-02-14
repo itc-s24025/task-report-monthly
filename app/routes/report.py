fa0eafaab2072ba2fcfb2a1cda82ea86a000f165
itc-s24013 <s24013@std.it-college.ac.jp>
2026-02-14 16:19:03 +0900

    GET /api/report/project 必要なデータ:なし 返すデータ "project_name" プロジェクト(タスク)名 "total_hour" 小数点一桁までの総作業時間(h) "progress" 小数点一桁までの進捗割合(%)
    時間計算の式も修正した

--- app/routes/report.py
+++ app/routes/report.py
@@ -6,10 +6,26 @@ from app import db
 
 report_bp = Blueprint('filter', __name__, url_prefix="/api/report")
 
-@report_bp.route("category", methods=['GET'])
-def categories():
-    category_all = db.session.query(Task.category_id).group_by(Task.category_id).all()
-    return redirect("categories.html", category_all=category_all)
+@report_bp.route("project", methods=["GET"])
+def category():
+    todo_list = (db.session.query(
+        func.sum(Task.duration_seconds).label('total_duration'),
+        func.sum(Task.end_time - Task.start_time).label('planned_duration')
+    ).group_by(
+        Task.id
+    ).order_by(func.sum(Task.duration_seconds).desc()).all())
+
+    return jsonify({
+        "data": [
+            {
+                "project_name": todo.task_name if todo.task_name else "名前がありません",
+                "total_hour": round(todo.total_duration / 3600, 1),
+                "progress": round(
+                    round(todo.planned_duration.total_seconds() / 3600, 1) / round(todo.total_duration / 3600,1)) * 100 if todo.planned_duration else 0
+            }
+            for todo in todo_list
+        ]
+    })
 
 @report_bp.route("category", methods=["GET"])
 def category():
@@ -24,8 +40,8 @@ def category():
         "data": [
             {
                 "category_name": todo.category.category_name if todo.category else "未分類",
-                "total_hour": round(todo.total_duration / 60 , 1),
-                "progress": round(round(todo.planned_duration.total_seconds() / 60 , 1) / round(todo.total_duration / 60 , 1)) * 100 if todo.planned_duration else 0
+                "total_hour": round(todo.total_duration / 3600 , 1),
+                "progress": round(round(todo.planned_duration.total_seconds() / 3600 , 1) / round(todo.total_duration / 3600 , 1)) * 100 if todo.planned_duration else 0
             }
             for todo in todo_list
         ]
@@ -52,6 +68,6 @@ def monthly():
     # (2026, 2, 3600.0)
 
     return jsonify({
-        "total_hour": round(month_todo.total_duration / 60 , 1) if month_todo else 0,
+        "total_hour": round(month_todo.total_duration / 3600 , 1) if month_todo else 0,
         "total_day": month_todo.total_day if month_todo else 0
     })
